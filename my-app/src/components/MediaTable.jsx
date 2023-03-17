486621d2a91a31525bc71b675b2892cde40ff40c
KaarleH <kaarleh@metropolia.fi>
2023-03-17 11:54:55 +0200

    hooks

--- my-app/src/components/MediaTable.jsx
+++ my-app/src/components/MediaTable.jsx
@@ -1,34 +1,8 @@
-import PropTypes from 'prop-types';
 import MediaRow from './MediaRow';
-import {useEffect, useState} from "react";
-import {baseUrl} from "../utils/variables.js";
+import {useMedia} from "../hooks/ApiHooks.js";
 
 const MediaTable = () => {
-
-  const [mediaArray, setMediaArray] = useState([]);
-  const getMedia = async () => {
-    const response = await fetch(baseUrl + 'media');
-    const files = await response.json();
-    const filesWithThumbnail = await Promise.all(
-      files.map(async (file)=>{
-        const response = await fetch(baseUrl + 'media/' + file.file_id);
-        return await response.json();
-      })
-    );
-    setMediaArray(filesWithThumbnail);
-  };
-
-  useEffect(()=>{
-    try {
-      getMedia();
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
-
-
-  console.log(mediaArray);
-
+  const {mediaArray} = useMedia();
   return (
     <table>
       <tbody>
