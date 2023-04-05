17f3f4219acc040ce787e21aaf488afd2c305411
KaarleH <kaarleh@metropolia.fi>
2023-04-05 20:16:59 +0300

    mui

--- my-app/src/views/Login.jsx
+++ my-app/src/views/Login.jsx
@@ -1,25 +1,31 @@
+import {Button, Grid, Typography} from '@mui/material';
 import PropTypes from 'prop-types';
+import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
-import {useState} from "react";
 
 const Login = (props) => {
-
   const [formToggle, setFormToggle] = useState(true);
   const toggle = () => {
     setFormToggle(!formToggle);
-  }
-
+  };
   return (
-    <>
-      {formToggle ?
-      <LoginForm />
-        :
-      <RegisterForm />
-      }
-      <p>{formToggle ? 'First time here?': 'Sign in'}</p>
-      <button onClick={toggle}>{formToggle ? 'Register': 'Login'}</button>
-    </>
+    <Grid container direction="column" alignItems="center">
+      <Grid item>
+        <Typography component="h1" variant="h3">
+          {formToggle ? 'Login' : 'Register'}
+        </Typography>
+      </Grid>
+      <Grid item>{formToggle ? <LoginForm /> : <RegisterForm />}</Grid>
+      <Grid item>
+        <Typography variant="body2" sx={{mt: 2}}>
+          {formToggle ? 'First time here?' : 'Already registered?'}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Button onClick={toggle}>{formToggle ? 'Register' : 'Login'}</Button>
+      </Grid>
+    </Grid>
   );
 };
 
